afc53ecd73403bd01e0b1a1fee96976d85801339
Caleb Gore <cgore2001@gmail.com>
2022-04-19 20:42:57 -0500

    making pottery at the wheel

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+// Imports go first
+import { makePottery } from './PotteryWheel.js'
+
+
+// Make 5 pieces of pottery at the wheel
+let mug1 = makePottery("mug", 5, 4)
+console.log(mug1);
+let mug2 = makePottery("mug", 5, 4)
+console.log(mug2);
+let mug3 = makePottery("mug", 5, 4)
+console.log(mug3);
+let mug4 = makePottery("mug", 5, 4)
+console.log(mug4);
+let mug5 = makePottery("mug", 5, 4)
+console.log(mug5);
+
+// Fire each piece of pottery in the kiln
+
+
+// Determine which ones should be sold, and their price
+
+
+// Invoke the component function that renders the HTML list
+
+
+
